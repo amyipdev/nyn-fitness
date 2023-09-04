d786f08355c3fca4acab644b4a8871f7c2b67dc3
Amy <apark0006@student.cerritos.edu>
2023-09-03 19:31:40 -0700

    Update App.svelte
    Test commit 
    
    Signed-off-by: Amy Parker <amy@amyip.net>

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -9,7 +9,7 @@
 	<div>
 		<Button outline color=danger>Test example bootstrap button</Button>
 	</div>
-	<h1>Hello {name}!</h1>
+	<h1>Hello there {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
