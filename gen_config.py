3885ab3f0ad199c24743dfd4dd8097bba55dd15f
Amy Parker <amy@amyip.net>
2023-09-09 11:33:56 -0700

    add database config to config gen
    This patch adds the ability to configure database settings to gen_config.py.
    
    Signed-off-by: Amy Parker <amy@amyip.net>

--- gen_config.py
+++ gen_config.py
@@ -3,5 +3,17 @@ import json
 config = dict()
 
 config["port"] = int(input("Port number: "))
+config["database"] = {
+    "host": input("Database IP address: "),
+    "port": int(x) \
+            if (x := input("Database port (leave blank for default): ")) != "" \
+            else 3306,
+    "db": input("Database name: "),
+    "pwd": input("Database password: ")
+}
+config["database"]["user"] = \
+    x if \
+    (x := input("Database user (if different from name): ")) != "" \
+    else config["database"]["db"]
 
 json.dump(config, open("nyn-conf.json", "w"))
